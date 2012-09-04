eb9674cd95008769f8b6631f832da5c68593dd52
Peter Graham <peter@1000memories.com>
2012-09-03 23:16:46 -0700

    ZoneEdit client for servers behind a NAT firewall

---
+++ zoneedit-dynamic-ip-client/client.rb
@@ -0,0 +1,10 @@
+require "open-uri"
+username = "YOUR_USERNAME"
+password = "YOUR_PASSWORD"
+hosts = "YOUR_HOSTS"
+
+open('http://ip.moe.ee') do |f|
+  ip = f.gets
+  auth_url = "https://dynamic.zoneedit.com/auth/dynamic.html?host=#{hosts}&dnsto=#{ip}"
+  `curl -s -u #{username}:#{password} '#{auth_url}'`
+end
